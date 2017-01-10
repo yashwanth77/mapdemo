4e98b394e87f751ca0b350a737519b348162e56c
Roger <Mavericks@Mavericks.local>
2017-01-10 11:25:04 -0600

    Initial Commit

---
+++ SampleMapDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SampleMapDemo
+//
+//  Created by Roger on 1/10/17.
+//  Copyright © 2017 Yashwanth. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SampleMapDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SampleMapDemo
+//
+//  Created by Roger on 1/10/17.
+//  Copyright © 2017 Yashwanth. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
